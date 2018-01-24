5397548147bfd5f5c5ae8ad54db7dbd8e448f72f
Abel Martín <abel.martin.ruiz@gmail.com>
2018-01-24 14:39:47 +0100

    Refactor code to use functions

--- redmine_notify.py
+++ redmine_notify.py
@@ -5,117 +5,134 @@ import notify2
 import yaml
 from os import path
 
-def build_message(issue):
-    """ Builds and format the notification message """
-    project_name = issue.project['name']
-    id = issue.id
-    subject = issue.subject
-    status_id = issue.status['id']
-    status_name = issue.status['name']
-    priority = issue.priority['name']
+def build_notification(issue, statuses_by_importance):
+    """ Builds and format the notification """
+    notification = { 'title':"", 'body':"", 'urgency':"" }
 
-    message = { 'title':"", 'body':"", 'urgency':"" }
+    notification['title'] = issue.project['name']
+    notification['body'] = '#' + str(issue.id) + ': ' + issue.subject + '\n' + '<i><b>' + \
+                      issue.priority['name'] + '</b> - ' + issue.status['name'] + '</i>'
 
-    message['title'] = project_name
-    message['body'] = '#' + str(id) + ': ' + subject + '\n' + '<i><b>' + \
-                      issue.priority['name'] + '</b> - ' + status_name + '</i>'
-
-    if status_id in critical_statuses:
-        message['urgency']='critical'
-    elif status_id in unimportant_statuses:
-        message['urgency']='low'
+    if issue.status['id'] in statuses_by_importance['critical_statuses']:
+        notification['urgency']='critical'
+    elif issue.status['id'] in statuses_by_importance['unimportant_statuses']:
+        notification['urgency']='low'
     else:
-        message['urgency']='normal'
+        notification['urgency']='normal'
 
-    return message
+    return notification
 
 
-def send_message(title, body, urgency='normal'):
-    """ Sends a notification message """
-    icon_filename='redmine.png'
+def send_notification(notification):
+    """ Sends a notification """
+    icon_file='redmine.png'
 
     urgency_level = {'low': 0,
                      'normal': 1,
                      'critical': 2}
 
     uri = path.join(path.dirname(path.abspath(__file__)),
-                       icon_filename)
+                       icon_file)
 
     notify2.init("")
-    notice = notify2.Notification(title, body, uri)
+    notice = notify2.Notification(notification['title'], notification['body'], uri)
 
-    notice.set_urgency(urgency_level[urgency])
+    notice.set_urgency(urgency_level[notification['urgency']])
 
     notice.show()
     return
 
-# config
-config_filename = 'redmine.yml'
-config = yaml.load(open(path.join(path.dirname(path.abspath(__file__)),
-                       config_filename)))
 
-redmine_url = config['url']
-redmine_username = config['username']
-redmine_password = config['password']
+def get_config(config_file):
+    """ Reads configuration from YAML file """
+    config = yaml.load(open(path.join(path.dirname(path.abspath(__file__)),
+                       config_file)))
+    return config
 
-redmine_instance = redmine.Redmine(redmine_url, username=redmine_username,
-                               password=redmine_password)
 
-user_id = redmine_instance.user.get('current').id
-statuses = redmine_instance.issue_status.all()
-projects = redmine_instance.project.all()
+def get_filters(config, all_statuses, all_projects):
+    """ Returns status, project and custom fields filters """
+    filtered_statuses = [status['id'] for status in all_statuses if status['name'] in
+                         config['status_filter']]
+    status_id_filter_str = '!' + '|'.join(str(status_id) for status_id in filtered_statuses)
+    
+    # filter by project
+    filtered_projects = [project['id'] for project in all_projects if project['name'] in
+                         config['project_filter']]
+    
+    # transform list of dicts format
+    # {'Remote': '0'} -> {'name': 'Remote', 'value': '0'}
+    # this is an inclusive filter
+    included_custom_fields = list()
+    for custom_field in config['custom_field_filter']:
+        for key in custom_field.keys():
+            included_custom_fields.append({'name': key, 'value': custom_field[key]})
 
-# fiter by status
-status_filter = config['status_filter']
-filtered_statuses = [status['id'] for status in statuses if status['name'] in
-                     status_filter]
-status_id_filter_str = '!' + '|'.join(str(status_id) for status_id in filtered_statuses)
+    return {'status_id_filter_str': status_id_filter_str,
+            'filtered_projects': filtered_projects,
+            'included_custom_fields': included_custom_fields}
 
-# filter by project
-project_filter = config['project_filter']
-filtered_projects = [project['id'] for project in projects if project['name'] in
-                     project_filter]
-# urgency by status
-status_urgency_critical = config['status_urgency_critical']
-critical_statuses = [status['id'] for status in statuses if status['name'] in
-                     status_urgency_critical]
+def get_statuses_by_importance(config, all_statuses):
+    """
+        Returns statuses classified by importance according to the
+        configuration file
+    
+    """
+    critical_statuses = [status['id'] for status in all_statuses if status['name'] in
+                         config['status_urgency_critical']]
 
-status_urgency_low = config['status_urgency_low']
-unimportant_statuses = [status['id'] for status in statuses if status['name'] in
-                     status_urgency_low]
+    unimportant_statuses = [status['id'] for status in all_statuses if status['name'] in
+                            config['status_urgency_low']]
 
-# transform list of dicts format
-# {'Remote': '0'} -> {'name': 'Remote', 'value': '0'}
-# this is an inclusive filter
-filtered_custom_fields = list()
-for custom_field in config['custom_field_filter']:
-    for key in custom_field.keys():
-        filtered_custom_fields.append({'name': key, 'value': custom_field[key]})
+    return {'critical_statuses': critical_statuses,
+            'unimportant_statuses': unimportant_statuses}
 
-# get sorted list of issues
-issues = sorted(redmine_instance.issue.filter(assigned_to_id=user_id,
-                                          status_id=status_id_filter_str),
-                key=lambda issue: issue.priority['id'],
-                reverse=True)
+def main():
+    config = get_config('redmine.yml')
+    
+    ## connect to redmine
+    redmine_instance = redmine.Redmine(config['url'], username=config['username'],
+                                   password=config['password'])
+    
+    user_id = redmine_instance.user.get('current').id
+    all_statuses = redmine_instance.issue_status.all()
+    all_projects = redmine_instance.project.all()
+    
+    filters = get_filters(config, all_statuses, all_projects)
+    status_id_filter_str = filters['status_id_filter_str']
+    filtered_projects = filters['filtered_projects']
+    included_custom_fields = filters['included_custom_fields']
+    
+    statuses_by_importance = get_statuses_by_importance(config, all_statuses)
+    critical_statuses = statuses_by_importance['critical_statuses']
+    unimportant_statuses = statuses_by_importance['unimportant_statuses']
+    
+    # get sorted list of issues
+    issues = sorted(redmine_instance.issue.filter(assigned_to_id=user_id,
+                                              status_id=status_id_filter_str),
+                    key=lambda issue: issue.priority['id'],
+                    reverse=True)
+    
+    for issue in issues:
+        project_id = issue.project['id']
+    
+        if project_id not in filtered_projects:
+            if hasattr(issue, 'custom_fields'):
+                # convert redmine.resources.CustomField object list to list of dicts
+                issue_custom_fields = list()
+                for custom_field in issue.custom_fields:
+                    issue_custom_fields.append({'name': custom_field['name'],
+                                                'value': custom_field['value']})
+    
+                for custom_field in included_custom_fields:
+                    if custom_field in issue_custom_fields:
+                        notification = build_notification(issue, statuses_by_importance)
+                        send_notification(notification)
+                        continue
+            #else:
+            #    notification = build_notification(issue)
+            #    send_notification(notification['title'], notification['body'],
+            #                 notification['urgency'])
 
-for issue in issues:
-    project_id = issue.project['id']
-
-    if project_id not in filtered_projects:
-        if hasattr(issue, 'custom_fields'):
-            # convert redmine.resources.CustomField object list to list of dicts
-            issue_custom_fields = list()
-            for custom_field in issue.custom_fields:
-                issue_custom_fields.append({'name': custom_field['name'],
-                                            'value': custom_field['value']})
-
-            for custom_field in filtered_custom_fields:
-                if custom_field in issue_custom_fields:
-                    message = build_message(issue)
-                    send_message(message['title'], message['body'],
-                                 message['urgency'])
-                    continue
-        #else:
-        #    message = build_message(issue)
-        #    send_message(message['title'], message['body'],
-        #                 message['urgency'])
+if __name__ == "__main__":
+    main()
